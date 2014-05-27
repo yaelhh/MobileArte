400d2eff32760eed2190edf8554b9fc5511461bc
Agustina <agustinap24@gmail.com>
2014-05-26 22:35:40 -0300

    Obra

--- src/com/example/clientarte/MainActivity.java
+++ src/com/example/clientarte/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.clientarte;
 
+import com.example.imageswitcher.R;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -9,30 +11,73 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.Toast;
 import android.os.Build;
+import android.widget.ViewSwitcher.ViewFactory;
+import android.app.ActionBar.LayoutParams;
 
 public class MainActivity extends ActionBarActivity {
 
+	private ImageButton img;
+	private ImageSwitcher imageSwitcher;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageButton)findViewById(R.id.imageButton1);
+        imageSwitcher = (ImageSwitcher)findViewById(R.id.Gallery02);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
+        imageSwitcher.setFactory(new ViewFactory() {
+			
+			@Override
+			public View makeView() {
+				ImageView myView = new ImageView(getApplicationContext());
+			      myView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+			      myView.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.
+			      FILL_PARENT,LayoutParams.FILL_PARENT));
+			      return myView;
+			}
+        });
+     }
+    
+    public void next(View view){
+        Toast.makeText(getApplicationContext(), "Next Image", 
+        Toast.LENGTH_LONG).show();
+        Animation in = AnimationUtils.loadAnimation(this,
+        android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this,
+        android.R.anim.slide_out_right);
+        imageSwitcher.setInAnimation(in);
+        imageSwitcher.setOutAnimation(out);
+        imageSwitcher.setImageResource(R.drawable.ic_launcher);
+     }
+     public void previous(View view){
+        Toast.makeText(getApplicationContext(), "previous Image", 
+        Toast.LENGTH_LONG).show();
+        Animation in = AnimationUtils.loadAnimation(this,
+        android.R.anim.slide_out_right);
+        Animation out = AnimationUtils.loadAnimation(this,
+        android.R.anim.slide_in_left);
+        imageSwitcher.setInAnimation(out);
+        imageSwitcher.setOutAnimation(in);
+        imageSwitcher.setImageResource(R.drawable.ic_launcher);
+     }
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+     }
+
+ 
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -57,7 +102,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.activity_main, container, false);
             return rootView;
         }
     }
