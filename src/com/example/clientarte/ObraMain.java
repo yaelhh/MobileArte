20ac3c774141926ba21dff60123dbcfe6bd897a2
Agustina <agustinap24@gmail.com>
2014-05-26 22:36:01 -0300

    Prueba

---
+++ src/com/example/clientarte/ObraMain.java
@@ -0,0 +1,14 @@
+package com.example.clientarte;
+
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageSwitcher;
+
+public class ObraMain extends ActionBarActivity{
+	
+	private ImageSwitcher imageSwitcher;
+	
+	
+
+
+
+}
