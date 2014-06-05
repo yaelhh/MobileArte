6e426ba0330b7eb3bfbad7e0a315d4b720fefc08
Yael <yaelhh@gmail.com>
2014-06-05 00:30:45 -0300

    Se creo el menu deslizable

--- src/com/example/clientarte/PerfilActivity.java
+++ src/com/example/clientarte/PerfilActivity.java
@@ -1,7 +1,5 @@
 package com.example.clientarte;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
@@ -10,7 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
 
 public class PerfilActivity extends Activity {
 
