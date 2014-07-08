f2516eb6381ab8cf43e2bca8caf0f079c8123b73
Yael <yaelhh@gmail.com>
2014-07-07 23:15:42 -0300

    cvcxvfdsfdsfds

---
+++ src/com/example/clientarte/NosotrosActivity.java
@@ -0,0 +1,20 @@
+package com.example.clientarte;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+
+public class NosotrosActivity extends  FragmentActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nosotros);
+
+		
+	}
+
+	
+}
